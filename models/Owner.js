303d725c5e21293817b3cb23c80932b7eae7b9a1
IsmailZekry <ismail.zekry7@gmail.com>
2022-05-18 23:51:13 +0200

    web finalized

--- models/Owner.js
+++ models/Owner.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Owner = new mongoose.Schema({
     name: {type: String, trim: true, default:''},
     password: {type: String, trim: true, default:''},
+    job_title: {type: String, trim: true, default:''},
     nationality: {type: String, trim: true, default:''},
     national_id: {type: String, trim: true, default:''},
     gender: {type: String, trim: true, default:''},
